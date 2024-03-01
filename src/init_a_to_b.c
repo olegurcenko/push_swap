401f5d44295eee0c6178da08a403129479d772fd
Oleh Yurchenko <oyurchen@c2r10s5.42prague.com>
2024-03-01 20:29:55 +0100

    -h

---
+++ src/init_a_to_b.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_a_to_b.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oyurchen <oyurchen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/01 19:08:26 by oyurchen          #+#    #+#             */
+/*   Updated: 2024/03/01 20:29:24 by oyurchen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/push_swap.h"
+
+void current_index(t_stack_node * stack)
+{
+	int	i;
+	int	median;
+	
+	i = 0;
+	if (!stack)
+		return ;
+	median = stack_len(stack) / 2;
+	while(stack)
+	{
+		stack->index = i;
+		if (i <= median)
+			stack->above_median = true;
+		else
+			stack->above_median = false;
+		stack = stack->next;
+		i++;
+	}
+}
+
+static void	set_target_a(t_stack_node *a, t_stack_node *b)
+{
+	t_stack_node	*current_b;
+	t_stack_node	*target_node;
+	long			best_match_index;
+
+	while (a)
+	{
+		best_match_index = LONG_MIN;
+		current_b = b;
+		while (current_b)
+		{
+			if (current_b->nbr < a->nbr && current_b->nbr > best_match_index)
+			{
+				best_match_index = current_b->nbr;
+				target_node = current_b;
+			}
+			current_b = current_b->next;
+		}
+		if (best_match_index == LONG_MIN)
+			a->target = find_max(b);
+		else
+			a->target = target_node;
+		a = a->next;
+	}
+}
+
+static void	cost_analysis_a(t_stack_node *a, t_stack_node *b)
+{
+	int	len_a;
+	int	len_b;
+
+	len_a = stack_len(a);
+	len_b = stack_len(b);
+	while (a)
+	{
+		a->push_cost = a->index;
+		if (!(a->above_median))
+			a->push_cost = len_a - (a->index);
+		if (a->target->above_median)
+			a->push_cost += a->target->index;
+		else
+			a->push_cost += len_b - (a->target->index);
+		a = a->next;
+	}
+	
+}
---
+++ src/sort_stacks.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_stacks.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oyurchen <oyurchen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/01 18:48:21 by oyurchen          #+#    #+#             */
+/*   Updated: 2024/03/01 18:53:54 by oyurchen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/push_swap.h"
+
+void	sort_stacks(t_stack_node **a, t_stack_node **b)
+{
+	int	len_a;
+
+	len_a = stack_len(*a);
+	if (len_a-- > 3 && !stack_sorted(*a))
+		pb(b, a, false);
+	if (len_a-- > 3 && !stack_sorted(*a))
+		pb(b, a, false);
+	while (len_a-- > 3 && !stack_sorted(*a))
+	{
+		init_nodes_a(*a, *b);
+	}
+}
